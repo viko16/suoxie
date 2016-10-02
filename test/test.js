b76dc271c3e7745e21a0895064be2d4b6c083e89
viko16 <16viko@gmail.com>
2016-10-02 17:29:49 +0800

    test: improve test

--- test/test.js
+++ test/test.js
@@ -2,16 +2,34 @@
 
 import test from 'ava'
 import suoxie from '../lib'
+import print from '../lib/print'
 
-test('main query', t => {
-  suoxie('internationalization')
-    .then(rst => {
-      t.truthy(rst)
-      t.is(typeof rst, 'object')
+test('single result', function * (t) {
+  const rst = yield suoxie('internationalization')
 
-      t.truthy(rst.results.result)
-      t.is(Object.prototype.toString.call(rst.results.result), '[object Array]')
+  t.truthy(rst)
+  t.is(typeof rst, 'object')
 
-      t.is(rst.results.result.term, 'I18N')
-    })
+  const usefulResult = rst.results.result
+  t.truthy(usefulResult)
+  t.is(typeof usefulResult, 'object')
+
+  t.is(rst.results.result.term, 'I18N')
+})
+
+test('multi result', function * (t) {
+  const rst = yield suoxie('repository')
+
+  t.truthy(rst)
+  t.is(typeof rst, 'object')
+
+  const usefulResult = rst.results.result
+  t.truthy(usefulResult)
+  t.is(Object.prototype.toString.call(usefulResult), '[object Array]')
+
+  t.is(rst.results.result[0].term, 'REPOS')
+})
+
+test('print', t => {
+  t.notThrows(print)
 })
